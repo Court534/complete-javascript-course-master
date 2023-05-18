b928faa366e7fe6c6dbdfba6f36619893c58ffd0
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-18 15:20:42 +0100

    Completed data set 1 and 2

--- 01-Fundamentals-Part-1/01-challenge.js
+++ 01-Fundamentals-Part-1/01-challenge.js
@@ -15,6 +15,7 @@
 // Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95m tall.
 // Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76m tall.
 
+// Data 1
 let heightMark = 1.69
 let weightMark = 78
 let markBMI = weightMark / heightMark ** 2 
@@ -23,6 +24,21 @@ console.log(markBMI)
 let heightJohn = 1.95
 let weightJohn = 92
 let johnBMI = weightJohn / heightJohn ** 2 
+console.log(johnBMI)
+
+markHigherBMI = markBMI > johnBMI
+
+console.log(markHigherBMI)
+
+// Data 1
+heightMark = 1.88
+weightMark = 95
+markBMI = weightMark / heightMark ** 2 
+console.log(markBMI)
+
+heightJohn = 1.76
+weightJohn = 85
+johnBMI = weightJohn / heightJohn ** 2 
 console.log(johnBMI)
 
 markHigherBMI = markBMI > johnBMI
