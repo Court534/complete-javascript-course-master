4ce36072ce5a8375583375cb5723c27be1642a67
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-19 13:38:15 +0100

    Getting use to JS functions

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,9 @@
+function fruitProcessor(apples, oranges) {
+  const juice = console.log(
+    `Juice with ${apples} apples and ${oranges} oranges`
+  );
+  return juice;
+}
+
+const appleJuice = fruitProcessor(10, 0);
+console.log(appleJuice);
