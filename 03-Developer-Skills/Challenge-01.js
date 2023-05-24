ef3c4730265d2d46d5414c9f5a2a75b883e7a79e
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-24 14:26:16 +0100

    Formatted code in challenge 1

--- 03-Developer-Skills/Challenge-01.js
+++ 03-Developer-Skills/Challenge-01.js
@@ -11,11 +11,10 @@ testData1 = [17, 21, 23];
 testData2 = [12, 5, -5, 0, 4];
 
 const printForecast = function (arr) {
-
   let str = "";
 
   for (let i = 0; i < arr.length; i++) {
-    str = str + `... ${arr[i]}c in ${i + 1} days `
+    str = str + `...${arr[i]}c in ${i + 1} days `;
   }
   return str;
 };
