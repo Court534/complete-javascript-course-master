dfcfdb7cc6e942620706d2dc93ca4977177e0d01
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-06-01 17:14:14 +0100

    multiple level distructuring

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,8 +12,8 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function(starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
   openingHours: {
@@ -32,21 +32,32 @@ const restaurant = {
   },
 };
 
-const arr = [1, 2, 3]
-const a = arr[0]
-const b = arr[2]
-const c = arr[3]
+const arr = [1, 2, 3];
+const a = arr[0];
+const b = arr[2];
+const c = arr[3];
 
 // Destructured array
-const [d, e, f] = arr
+const [d, e, f] = arr;
 console.log(d, e, f); // OUTPUT: 1 2 3
 
 // Lets take some code from the restaurant object above
 // const [first, second] = restaurant.categories
 // console.log(first, second); // OUTPUT: Italian Pizzeria
 
-// If you need to skip an item 
-const [first, ,second] = restaurant.categories
+// If you need to skip an item
+const [first, , second] = restaurant.categories;
 console.log(first, second); // OUTPUT: Italian Vegetarian
 
-console.log(restaurant.order(2, 0))
+// Destructuring 2 return values from the order function
+const [starter, main] = restaurant.order(2, 0);
+console.log(starter, main);
+
+// Nested arrays
+const nested = [2, 4, [5, 6]]
+// const [i, , j] = nested
+// console.log(i, j); // OUTPUT: 2 [ 5, 6 ]
+
+// Destructuring on multiple levels
+const [i, , [j, k]] = nested
+console.log(i, j, k); // OUTPUT: 2 5 6
