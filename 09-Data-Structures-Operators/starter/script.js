f1566990fe27835e4d71eeac150488a007fe4aa0
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-06-29 15:03:25 +0100

    commented out a couple bit of code

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -64,27 +64,28 @@ const restaurant = {
     console.log(otherIng);
   },
 };
+
 // With optional chaining
-console.log(restaurant.openingHours.fri?.open);
+// console.log(restaurant.openingHours.fri?.open);
 
-const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
-for (const day of days) {
-  const open = restaurant.openingHours[day]?.open ?? 'closed';
-  console.log(`On ${day} we open at ${open}`);
-}
+// for (const day of days) {
+//   const open = restaurant.openingHours[day]?.open ?? 'closed';
+//   console.log(`On ${day} we open at ${open}`);
+// }
 
 // Methods 
 
 // Existing methods 
-console.log(restaurant.order?.(0,1) ?? "Method does not exist");
+// console.log(restaurant.order?.(0,1) ?? "Method does not exist");
 
 // Non-existing methods
-console.log(restaurant.orderPizza?.(0,1) ?? "Method does not exist");
+// console.log(restaurant.orderPizza?.(0,1) ?? "Method does not exist");
 
 // Arrays
-const users = [{name: "Courtney", email: "courtney@hi.com"}]
-console.log(users[0]?.name ?? 'Users array empty');
+// const users = [{name: "Courtney", email: "courtney@hi.com"}]
+// console.log(users[0]?.name ?? 'Users array empty');
 
 
 
