5bf1b551f3e4645bd0fa3fbb2d122200754c6adc
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-06-29 15:01:52 +0100

    finished the module on optional chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -35,7 +35,7 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  // Old code 
+  // Old code
   // order: function (starterIndex, mainIndex) {
   //   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   // },
@@ -64,6 +64,30 @@ const restaurant = {
     console.log(otherIng);
   },
 };
+// With optional chaining
+console.log(restaurant.openingHours.fri?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day} we open at ${open}`);
+}
+
+// Methods 
+
+// Existing methods 
+console.log(restaurant.order?.(0,1) ?? "Method does not exist");
+
+// Non-existing methods
+console.log(restaurant.orderPizza?.(0,1) ?? "Method does not exist");
+
+// Arrays
+const users = [{name: "Courtney", email: "courtney@hi.com"}]
+console.log(users[0]?.name ?? 'Users array empty');
+
+
+
 
 // restaurant.orderDelivery({
 //   time: '22:30',
